9d945573fa3c99bc6942ee4616581d3ccf51a0a4
Владислав Береснев <vladislaw.beresnev2018@yandex.ru>
2023-02-22 23:32:51 +0700

    первый коммит 20 спринта

--- yacut/black_list.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-
-def generate_short_id(length=16):
-    chars = string.ascii_letters + string.digits
-    return ''.join(random.choice(chars) for _ in range(length))
-
-print(generate_short_id())
